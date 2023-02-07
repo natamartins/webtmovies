0e0c039d081952f2b39b00c38e3c3f1f283c2bd3
natamartins <nata.codedev@gmail.com>
2023-02-07 18:44:05 -0300

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  fetch(process.env.REACT_APP_URL || 'http://localhost:8080/api/v1')
-    .then(() => {
-      console.log("Deu certo")
-    })
-    .catch(() => {
-      console.log("Deu errado")
-    })
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>webTmovies</h1>
     </div>
   );
 }
